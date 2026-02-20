4bd567ae44dd109cd09cf387311588516790031b
admin <admin@TeamCity>
2026-02-20 02:39:18 +0000

    build step was copied in 'Build' build configuration (TeamCity change in 'NETOLOGY' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -50,6 +50,17 @@ object Build : BuildType({
             runnerArgs = "-Dmaven.test.failure.ignore=true"
             userSettingsSelection = "settings.xml"
         }
+        maven {
+            name = "Maven Master Deploy (1)"
+            id = "Maven2_1"
+
+            conditions {
+                contains("teamcity.build.branch", "master")
+            }
+            goals = "clean deploy"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            userSettingsSelection = "settings.xml"
+        }
     }
 
     features {
